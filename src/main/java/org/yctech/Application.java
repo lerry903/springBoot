35719d06c2d096106088739ec923a66b04f83b5c
shyctech <lerryli@foxmail.com>
2016-10-19 17:27:11 +0800

    no msg

--- src/main/java/org/yctech/Application.java
+++ src/main/java/org/yctech/Application.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
-import org.yctech.common.GlobalConstant;
-import org.yctech.resource.ResourceBox;
 
 @Configuration
 @EnableAutoConfiguration
