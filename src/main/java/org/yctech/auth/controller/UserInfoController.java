2c0b0dfa80fea3db7d8e69f5fa732afb50870eb0
shyctech <lerryli@foxmail.com>
2016-10-21 17:31:28 +0800

    增加Spring security权限验证

--- src/main/java/org/yctech/auth/controller/UserInfoController.java
+++ src/main/java/org/yctech/auth/controller/UserInfoController.java
@@ -3,6 +3,7 @@ package org.yctech.auth.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,10 +26,12 @@ public class UserInfoController {
     private UserInfoRepository userInfoRepo;
 
     @RequestMapping("/{id}")
+    @Secured("ROLE_USER")
     public UserInfo findUserById(@PathVariable("id") Long id) {
         logger.info("UserId :"+id);
         UserInfo userInfo = userInfoRepo.findOne(id);
         return userInfo;
     }
+
 
 }
